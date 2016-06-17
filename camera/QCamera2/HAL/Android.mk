3f2faf662c3328bd7b3c78d187763d122067149c
pirej <pirej30@yahoo.com>
2016-06-17 19:24:34 +0200

    update camera hal to LA.BR.1.2.7_rb1.22

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -1,5 +1,4 @@
 LOCAL_PATH:= $(call my-dir)
-
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
         QCamera2Factory.cpp \
@@ -18,11 +17,28 @@ LOCAL_SRC_FILES := \
         wrapper/QualcommCamera.cpp
 
 LOCAL_CFLAGS = -Wall -Wextra -Werror -DDEFAULT_DENOISE_MODE_ON
+LOCAL_CFLAGS += -DHAS_MULTIMEDIA_HINTS
+
+#Debug logs are enabled
+#LOCAL_CFLAGS += -DDISABLE_DEBUG_LOG
+
+#ifeq ($(TARGET_USE_VENDOR_CAMERA_EXT),true)
+#LOCAL_CFLAGS += -DUSE_VENDOR_CAMERA_EXT
+#endif
+
+#ifeq ($(TARGET_USES_AOSP),true)
+#LOCAL_CFLAGS += -DVANILLA_HAL
+#endif
+
+ifneq ($(call is-platform-sdk-version-at-least,18),true)
+LOCAL_CFLAGS += -DUSE_JB_MR1
+endif
 
 LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../stack/common \
         frameworks/native/include/media/openmax \
         $(call project-path-for,qcom-display)/libgralloc \
+        $(call project-path-for,qcom-display)/libqdutils \
         $(call project-path-for,qcom-media)/libstagefrighthw \
         system/media/camera/include \
         $(LOCAL_PATH)/../../mm-image-codec/qexif \
@@ -30,16 +46,37 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../util \
         $(LOCAL_PATH)/wrapper
 
+ifeq ($(call is-platform-sdk-version-at-least,20),true)
+LOCAL_C_INCLUDES += system/media/camera/include
+else
+LOCAL_CFLAGS += -DUSE_KK_CODE
+endif
+
+#ifeq ($(TARGET_USE_VENDOR_CAMERA_EXT),true)
+#LOCAL_C_INCLUDES += hardware/qcom/display/msm8974/libgralloc
+#else
+#LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
+#endif
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
+ifeq ($(TARGET_TS_MAKEUP),true)
+LOCAL_CFLAGS += -DTARGET_TS_MAKEUP
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/tsMakeuplib/include
+endif
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl
-LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libqdMetaData
+ifeq ($(TARGET_TS_MAKEUP),true)
+LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal
+endif
+LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_32_BIT_ONLY := true
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+ifeq ($(TARGET_USES_AOSP),false)
+include $(LOCAL_PATH)/test/Android.mk
+endif
